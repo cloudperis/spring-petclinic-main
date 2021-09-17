91cdc42941c55b4e43390fae96ed937ff258c144
ayowole1144 <ayowole1144@gmail.com>
2021-09-17 16:15:28 -0300

    jnjkn

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -37,7 +37,7 @@ pipeline{
                       accessKeyVariable: 'AWS_ACCESS_KEY_ID',
                       secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) 
                         {
-                          sh "aws ec2 reboot-instances --instance -ids ${params.devserver} --region us-east-1"
+                          sh "aws ec2 reboot-instances --instance-ids ${params.devserver} --region us-east-1"
                         }
             }
         }
