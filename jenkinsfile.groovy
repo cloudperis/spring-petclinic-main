ea5eb0eab427f0244f4d63cc2b395517cef1d597
olulegend88888 <bodeige81@gmail.com>
2023-07-26 12:24:50 -0500

    bbb

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -45,7 +45,7 @@ pipeline {
                       $class: 'AmazonWebServicesCredentialsBinding',
                       credentialsId: "jenkinss3",
                       accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-                      secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) 
+                      secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]){ 
 
 
                           sh "aws ec2 reboot-instances --instance-ids i-09759a80862886d80 --region us-east-2"
